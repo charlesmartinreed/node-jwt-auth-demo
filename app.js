3bdc8d216d751554e5819bb2b7d59dcb44a0c4d4
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-15 16:45:35 -0500

    Demonstrate protected post route using JWT Bearer token authorization.

--- app.js
+++ app.js
@@ -10,11 +10,19 @@ app.get("/api", (req, res) => {
   });
 });
 
-// will be protected
-app.post("/api/post", (req, res) => {
-  res.json({
-    message: "Post created..."
-  });
+// protected route - only allowed access if token matches wht is expected for the user
+app.post("/api/post", verifyToken, async (req, res) => {
+  try {
+    const authData = await jwt.verify(req.token, "secretKey");
+    // timestamp is iat, "issued at"
+    res.json({
+      message: "Post created...",
+      authData
+    });
+  } catch (err) {
+    res.sendStatus(403);
+    console.log(err);
+  }
 });
 
 app.post("/api/login", async (req, res) => {
@@ -34,6 +42,32 @@ app.post("/api/login", async (req, res) => {
     console.log(err);
   }
 });
+
+/*
+Format of Token
+Authorization: Bearer <access_token>
+*/
+
+// token verification middleware
+function verifyToken(req, res, next) {
+  // calling next sends us to the the next middleware in the line
+
+  // Get auth header value
+  const bearerHeader = req.headers["authorization"];
+
+  // check that the bearer is not undefined
+  if (typeof bearerHeader !== "undefined") {
+    // take the token out of the bearer by splitting at the space
+    // second value in the array will be the token
+    const bearerToken = bearerHeader.split(" ")[1];
+    // set the Token
+    req.token = bearerToken;
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+}
 
 app.listen(5000, () => {
   console.log("App started on port 5000");
